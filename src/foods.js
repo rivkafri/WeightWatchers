c6283ef66e740eccfeea8cebc836745fd3aea326
rivki <r0533145417@gmail.com>
2022-06-30 09:39:17 +0300

    add meeting page and requsets to json and write diary page

--- src/foods.js
+++ src/foods.js
@@ -1,56 +1,135 @@
 const SearchFoods = document.querySelector('#SearchFoods');
 const search = document.querySelector('#search');
-const container = document.querySelector('.usersTable');
-
+let container = "";
 let array;
 
 showDetails = (p) => {
     console.log(p);
     container.innerHTML = "";
-    // const btnBack = document.createElement("button");
-    // btnBack.innerHTML = "אחורה";
-    // btnBack.addEventListener("click", () => creatTable());
     let table = '';
-    table += `
+    if (hebrew) {
+        table += `
         <tr>
             <th><button type="submit" onclick="creatTable()">לחזרה לעמוד הקודם</button><th>
             <th>protein: ${p.protein}</th>
         </tr>`
-    // container.append(btnBack);
+    }
+    else {
+        table += `
+        <tr>
+            <th><button type="submit" onclick="creatTable()">לחזרה לעמוד הקודם</button><th>
+            <th>protein: ${p.protein_g}</th>
+        </tr>`
+    }
+    container = document.querySelector('.usersTable');
     container.innerHTML += table;
 }
 
 const creatTable = () => {
     container.innerHTML = "";
     console.log(array);
-    array.forEach((product) => {
-        const div = document.createElement('div');
-        const h2 = document.createElement('h2');
-        h2.innerHTML = product.shmmitzrach;
-        div.append(h2);
-        const btn = document.createElement('button');
-        btn.innerHTML = "ערכים תזונתיים";
-        btn.addEventListener("click", () => showDetails(product));
-        div.append(btn);
-        container.append(div);
-    });
-    // container = document.querySelector('.usersTable');
-    // container.innerHTML += table;
+    if (array == [])
+        alert("not exist!");
+    else {
+        array.forEach((product) => {
+            const div = document.createElement('div');
+            const h2 = document.createElement('h2');
+            if (hebrew)
+                h2.innerHTML = product.shmmitzrach;
+            else
+                h2.innerHTML = product.name;
+            div.append(h2);
+            const btn = document.createElement('button');
+            btn.innerHTML = "ערכים תזונתיים";
+            btn.addEventListener("click", () => showDetails(product));
+            div.append(btn);
+            container.append(div);
+        });
+    }
 }
 
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '071fae810amshb706a2052da6b94p190cadjsnf53c140ff392',
+        'X-RapidAPI-Host': 'calorieninjas.p.rapidapi.com'
+    }
+};
+
+const contains_heb = (str) => {
+    return (/[\u0590-\u05FF]/).test(str);
+}
+
+let hebrew = false;
 SearchFoods.onsubmit = (e) => {
+    hebrew = false;
     e.preventDefault();
     console.log(search.value);
-    const req = fetch(`https://data.gov.il/api/3/action/datastore_search?resource_id=c3cb0630-0650-46c1-a068-82d575c094b2&q=${search.value}`)
-    req.then(response => response.json())
-        .then(response => {
-            console.log(response);
-            array = "";
-            array = response.result.records;
-            creatTable();
+    if (search.value != "") {
+        if (contains_heb(search.value)) {
+            hebrew = true;
+            const req = fetch(`https://data.gov.il/api/3/action/datastore_search?resource_id=c3cb0630-0650-46c1-a068-82d575c094b2&q=${search.value}`)
+            req.then(response => response.json())
+                .then(response => {
+                    console.log(response);
+                    array = "";
+                    array = response.result.records;
+                    creatTable();
+                }
+                ).catch(err => console.error(err));
         }
-        )
-        .catch(err => console.error(err));
+        else {
+            const req = fetch(`https://calorieninjas.p.rapidapi.com/v1/nutrition?query=${search.value}`, options)
+            req
+                .then(response => response.json())
+                .then(response => {
+                    console.log(response.items);
+                    array = "";
+                    array = response.items;
+                    creatTable();
+                }
+                ).catch(err => console.error(err));
+        }
+    } else {
+        array = "";
+        container.innerHTML = "";
+        alert("enter again!");
+    }
 }
 
+
+
+     // const req = fetch(`https://calorieninjas.p.rapidapi.com/v1/nutrition?query=${search.value}`, options)
+        // req
+        //     .then(response => response.json())
+        //     .then(response => console.log(response.items[0]))
+        //     //.then(response => document.getElementById('showNutritionalValues').innerHTML=JSON.stringify(response.items[0]))
+
+
+
 // https://chomp.p.rapidapi.com/request.php?ingredient=${q.value}
+
+
+
+// const options = {
+//     method: 'GET',
+//     headers: {
+//         'X-RapidAPI-Key': '071fae810amshb706a2052da6b94p190cadjsnf53c140ff392',
+//         'X-RapidAPI-Host': 'calorieninjas.p.rapidapi.com'
+//     }
+// };
+// function getNutritionalValues(){
+//     const food = document.getElementById('nutritionalValues').value;
+//     fetch(`https://calorieninjas.p.rapidapi.com/v1/nutrition?query=${food}`, options)
+//     .then(response => response.json())
+//     .then(response => console.log(response.items[0]))
+//     //.then(response => document.getElementById('showNutritionalValues').innerHTML=JSON.stringify(response.items[0]))
+//     .catch(err => console.error(err));
+// }
+
+
+    // const btnBack = document.createElement("button");
+    // btnBack.innerHTML = "אחורה";
+    // btnBack.addEventListener("click", () => creatTable());
+
+        // container.append(btnBack);
---
+++ src/meeting.js
@@ -0,0 +1,54 @@
+const form = document.querySelector('#form');
+let obj = {};
+let arrMeetings = [];
+
+const fetchGet = () => {
+    fetch('http://localhost:3000/users')
+        .then(response => {
+            return response.json();
+        }).then(data => {
+            console.log(data);
+            data.forEach(user => {
+                arrMeetings = user.weight.meetings;
+                creatTableMeeting(user);
+            });
+        })
+};
+fetchGet();
+
+let usersMeeting = "";
+creatTableMeeting = (user) => {
+    let table = '';
+    table += `
+                <tr>
+                    <th><input  type="text" value=${user.firstName + user.lastName}></th>
+                    <th><input  type="text" value=${user.weight.meetings[user.weight.meetings.length - 1].weight}></th>
+                    <th><input  type="Date"  value=${new Date().toISOString().split("T")[0]}></th>
+                    <th><input  type="text" placeholder="write a comments"/></th>
+                    <th><input type="checkbox"/></th>
+                </tr>`
+    usersMeeting = document.querySelector('.usersMeeting');
+    usersMeeting.innerHTML += table;
+}
+
+saveInJson = (obj) => {
+    console.log(obj);
+    arrMeetings.push(obj);
+    console.log(arrMeetings);
+    fetch(`http://localhost:3000/users/${userURL}`, {
+        method: `PATCH`,
+        // body: JSON.stringify(obj),
+        body: JSON.stringify({
+            "weight.meetings": arrMeetings,
+        }),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    }).then(response => console.log(response));
+    arrMeetings = [];
+}
+
+form.onsubmit = (e) => {
+    e.preventDefault();
+    obj = { date: , weight: , comments: , notVisit:  }
+}
